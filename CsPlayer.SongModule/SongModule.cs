1af830991a9f1f8b51ef3539dd6698317af97527
P H <ph1387@t-online.de>
2018-07-14 03:01:52 +0200

    Added song module to solution

---
+++ CsPlayer.SongModule/SongModule.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPlayer.SongModule
+{
+    public class SongModule : IModule
+    {
+        public void Initialize()
+        {
+            this.InitializeServices();
+            this.InitializeViews();
+        }
+
+        private void InitializeServices()
+        {
+            
+        }
+
+        private void InitializeViews()
+        {
+            
+        }
+    }
+}
