aea5c6049841519360ff084d60c098c5d1faa92e
P H <ph1387@t-online.de>
2018-07-14 02:34:26 +0200

    Added region names as separate project and mapped them

---
+++ CsPlayer.Regions/RegionNames.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPlayer.Regions
+{
+    public static class RegionNames
+    {
+        public static readonly string TabRegion = "TabRegion";
+
+        public static readonly string MainRegion = "MainRegion";
+    }
+}
