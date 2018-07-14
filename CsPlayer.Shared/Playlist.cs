d03cf904d5472bb49696ec2a8dcd0534be60a785
P H <ph1387@t-online.de>
2018-07-14 22:41:53 +0200

    Added shared types project

---
+++ CsPlayer.Shared/Playlist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPlayer.Shared
+{
+    public class Playlist
+    {
+        public string Name { get; set; }
+        public bool Valid { get; set; }
+        public List<Song> Songs { get; private set; }
+
+        public Playlist(string name, bool valid = true)
+        {
+            Name = name;
+            Valid = valid;
+            Songs = new List<Song>();
+        }
+
+        public bool Verify()
+        {
+            var noInvalids = Songs.Select(x => x.Verify()).All(x => x);
+            Valid = noInvalids;
+
+            return noInvalids;
+        }
+    }
+}
---
+++ CsPlayer.Shared/Song.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPlayer.Shared
+{
+    public class Song
+    {
+        public string FilePath { get; set; }
+        public bool Valid { get; set; }
+
+        public Song(string filePath, bool valid = true)
+        {
+            FilePath = filePath;
+            Valid = valid;
+        }
+
+        public bool Verify()
+        {
+            var exists = File.Exists(FilePath);
+            Valid = exists;
+
+            return exists;
+        }
+    }
+}
