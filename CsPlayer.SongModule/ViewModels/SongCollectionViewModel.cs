190725d7df764fb4c630b9c5be17d6b908820b98
P H <ph1387@t-online.de>
2018-07-14 05:39:27 +0200

    Added basic design of song tab view

--- CsPlayer.SongModule/ViewModels/SongCollectionViewModel.cs
+++ CsPlayer.SongModule/ViewModels/SongCollectionViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,13 @@ namespace CsPlayer.SongModule.ViewModels
         {
             get { return _header; }
             private set { SetProperty<string>(ref _header, value); }
+        }
+
+        private ObservableCollection<string> _displayedSongPaths;
+        public ObservableCollection<string> DisplayedSongPaths
+        {
+            get { return _displayedSongPaths; }
+            set { SetProperty<ObservableCollection<string>>(ref _displayedSongPaths, value); }
         }
 
         public SongCollectionViewModel()
