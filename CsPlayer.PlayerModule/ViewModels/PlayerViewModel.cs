79c924a7bdc1eee5bed0aa940edf79157eca5de1
P H <ph1387@t-online.de>
2018-07-15 09:01:30 +0200

    Added basic player ListView template, added commands in viewmodel

--- CsPlayer.PlayerModule/ViewModels/PlayerViewModel.cs
+++ CsPlayer.PlayerModule/ViewModels/PlayerViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using CsPlayer.Shared;
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CsPlayer.PlayerModule.ViewModels
 {
@@ -17,7 +19,51 @@ namespace CsPlayer.PlayerModule.ViewModels
             private set { SetProperty<PlaylistViewModel>(ref _playlist, value); }
         }
 
+        public ICommand ButtonPrevious { get; private set; }
+        public ICommand ButtonPlay { get; private set; }
+        public ICommand ButtonPause { get; private set; }
+        public ICommand ButtonStop { get; private set; }
+        public ICommand ButtonNext { get; private set; }
+        public ICommand ButtonSaveChanges { get; private set; }
+
         public PlayerViewModel()
+        {
+            ButtonPrevious = new DelegateCommand(this.ButtonPreviousClicked);
+            ButtonPlay = new DelegateCommand(this.ButtonPlayClicked);
+            ButtonPause = new DelegateCommand(this.ButtonPauseClicked);
+            ButtonStop = new DelegateCommand(this.ButtonStopClicked);
+            ButtonNext = new DelegateCommand(this.ButtonNextClicked);
+            ButtonSaveChanges = new DelegateCommand(this.ButtonSaveChangesClicked);
+        }
+
+
+        // ---------- Buttons
+        private void ButtonPreviousClicked()
+        {
+
+        }
+
+        private void ButtonPlayClicked()
+        {
+
+        }
+
+        private void ButtonPauseClicked()
+        {
+
+        }
+
+        private void ButtonStopClicked()
+        {
+
+        }
+
+        private void ButtonNextClicked()
+        {
+
+        }
+
+        private void ButtonSaveChangesClicked()
         {
 
         }
